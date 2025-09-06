8efa2122e07e8ef1479010cd139191c77c5154e4
Benson Chen <tumlivein@gmail.com>
2025-09-06 17:30:42 +0800

    feat: 2. Shadow DOM - innerHTML not work on Shadow DOM

--- todo.js
+++ todo.js
@@ -1,7 +1,17 @@
+const template = document.createElement("template");
+template.innerHTML = `
+  <style>
+    h3 { color: green; }
+  </style>
+  <h3 data-title=""></h3>
+`;
 class TodoItem extends HTMLElement {
   constructor() {
     super();
-    this.innerHTML = `<style>h3 { color: green; }</style><h3>${this.innerHTML}</h3>`;
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.appendChild(template.content.cloneNode(true));
+    this.title = shadow.querySelector("[data-title]");
+    this.title.innerHTML = this.innerHTML;
   }
 }
 
