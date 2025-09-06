f900d9507a5cfb1179c6470d6f8b484ef89aafc7
Benson Chen <tumlivein@gmail.com>
2025-09-06 17:40:37 +0800

    feat: 3. Slot - slot with name

--- todo.js
+++ todo.js
@@ -5,10 +5,18 @@ template.innerHTML = `
       color: #099;
       display: block;
     }
+
+    .description {
+      font-size: 12px;
+      color: #999;
+    }
   </style>
   <label>
     <input type="checkbox" />
     <slot></slot>
+    <span class="description">
+      <slot name="description"></slot>
+    </span>
   </label>
 `;
 class TodoItem extends HTMLElement {
